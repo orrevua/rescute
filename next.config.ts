cbc65a40d1a307615b65a82ac934a32108f3eb79
Felipe França <felipe.franca.108@ufrn.edu.br>
2026-06-26 00:41:27 -0300

    fix(config): allow private IP for next/image in local dev

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowLocalIP: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
       },
     ],
   },
